a4839e8a19868ea20c83cbb0963cb483b28fce31
mohammad1384g <mmirghaderi4@gmail.com>
2025-10-18 22:27:27 +0330

    Update SJF_project.py

--- SJF_project.py
+++ SJF_project.py
@@ -14,8 +14,6 @@ def data():
       pid =i + 1
       arival_time=int(input(f"A.T  {pid}: "))
       execut_time=int(input(f"E.T  {pid} :"))
-
-
       processes.append({
             "pid": pid,
             "A.T": arival_time, 
@@ -27,7 +25,6 @@ def data():
         })
     return processes
     
-
 def sjf_sort_do(processes):
     processes.sort(key=lambda p : p["E.T"])
     
@@ -35,9 +32,7 @@ def sjf_sort_do(processes):
     finished = 0
     n = len(processes)
     gant_chart = []
-
     print("\n+++++ SJF Processes")
-
     while  finished != n :
         ready_processes = []
         for p in processes:
@@ -47,73 +42,53 @@ def sjf_sort_do(processes):
         if len(ready_processes) == 0:
             current_time += 1
             continue
-
         shortest_job = min(ready_processes, key=lambda x: x["E.T"])
-
         # do it processes 
         shortest_job["S.T"] = current_time
         shortest_job["end_time"] = current_time + shortest_job["E.T"]
         shortest_job["W.T"] = shortest_job["S.T"] - shortest_job["A.T"]
         shortest_job["finished"] = True
-
         print(f"time {current_time} : p{shortest_job["pid"]} started")
         print(f"time {shortest_job['end_time']}: P{shortest_job['pid']} finished")
-
-
         gant_chart.append({
           "pid": shortest_job["pid"],
           "start": shortest_job["S.T"],
           "end": shortest_job["end_time"]
     })
     
-
         current_time = shortest_job["end_time"]
         finished+=1
         
     return gant_chart
-
 def result(processes):
    print("++++++Final result+++++")
    print("PID\tA.T\tE.T\t S.T\tW.T")
    print("+-"*50)
-
    total_wating=0
    jam_zaman_kol = 0
    for p in processes:
-      jam_zaman_kol = p["end_time"] - p["A.T"]
+      turnaround_time = p["end_time"] - p["A.T"]
+      jam_zaman_kol+= turnaround_time
       print(f"{p["pid"]} \t {p["A.T"]} \t {p["E.T"]} \t {p["S.T"]} \t {p["W.T"]} ")
-
-      jam_zaman_kol+= jam_zaman_kol
       total_wating+=p["W.T"] 
-      avg_jam_zaman_kol = jam_zaman_kol / len(processes)
 
+   avg_jam_zaman_kol = jam_zaman_kol / len(processes)
    print("-" * 60)
    print(f"The Avrage wating time is : {total_wating / len(processes)}")
    print(f"Average jam_zaman_kol Time: {avg_jam_zaman_kol:.2f}")
-
-
 def gant_chart(gant_chart):
    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Gant chart<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-
-
-
    for g in gant_chart:
       print(f"|  P{g['pid']}  ", end="")
    print("|")
-
-
    for t in gant_chart:
-        print(f"{g['start']:<7}", end="")
+        print(f"{t['start']:<7}", end="")
    print(f"{gant_chart[-1]['end']}")
-
-
-
 print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<+SJF (Shortest Job First) Algorithm+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 processes = data()
 if processes:
    gant=sjf_sort_do(processes)
    result(processes)
-   gant_chart=gant_chart(processes)
+   gant_chart(gant)
 else:
    print("No data entered!")
-   
